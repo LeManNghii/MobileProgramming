23e1ce85ac7d89bc5fdedffe763e7e9bf543556e
H82M19\Student <Student>
2025-08-27 07:48:19 +0700

    chinh sua cau 1

--- LeManNghi_22652921_Tuan2/dist/promise.js
+++ LeManNghi_22652921_Tuan2/dist/promise.js
@@ -2,6 +2,9 @@
 // 1. Create a Promise that returns the string "Hello Async" after 2 seconds.
 const promise1 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        console.log("Hello Async");
+        resolve("Hello Async");
     }, 2000);
+});
+promise1.then(value => {
+    console.log(value);
 });
--- LeManNghi_22652921_Tuan2/src/promise.ts
+++ LeManNghi_22652921_Tuan2/src/promise.ts
@@ -1,6 +1,10 @@
 // 1. Create a Promise that returns the string "Hello Async" after 2 seconds.
 const promise1 = new Promise((resolve, reject) =>{
     setTimeout(() => {
-        console.log("Hello Async");
+        resolve("Hello Async");
     },2000);
 });
+
+promise1.then(value => {
+    console.log(value);
+})
